8248ce14d1e12728d7a0bdf768a55b852780aeaf
Mohammad <mohamad.ma09@gmail.com>
2020-04-09 00:57:35 -0500

    boilerplate, shared components, home, MAT UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './shared/app/app.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    FooterComponent,
+    SideBarComponent,
+    HeaderComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

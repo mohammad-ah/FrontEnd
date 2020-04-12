99f61e57c66966a12a6d49aa0642dfb1c01a8775
Ammar Atef <ammar.atef45@gmail.com>
2020-04-12 14:50:36 -0500

    added admin service

--- src/app/components/admin/filtered/filtered.component.ts
+++ src/app/components/admin/filtered/filtered.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-filtered',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilteredComponent implements OnInit {
 
-  constructor() { }
+  constructor(private adminService: AdminService) { }
 
   ngOnInit(): void {
+    this.adminService.getFiltered();
   }
 
 }
---
+++ src/app/services/admin.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor(private httpClient: HttpClient ) { }
+
+  getFiltered() {
+    this.httpClient.get('http://localhost:3000/admin/filtered')
+    .subscribe(
+      data => console.log(data),
+      error => console.log(error)
+    );
+  }
+}
